cb7b09238b775ae0c2b299e80825c0ed6de35b29
CoderRaushan <raushankumar23082004@gmail.com>
2026-09-19 10:45:05 +0530

    updated req file

--- google_search_agent.py
+++ google_search_agent.py
@@ -2,11 +2,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_community.utilities import GoogleSerperAPIWrapper 
-from langchain_groq import ChatGroq 
+from langchain_google_genai import ChatGoogleGenerativeAI 
 from langchain.agents import create_agent 
 from langgraph.checkpoint.memory import InMemorySaver  
 
-llm=ChatGroq(model="openai/gpt-oss-20b")
+llm=ChatGoogleGenerativeAI(model="gemini-3.6-flash")
 
 search=GoogleSerperAPIWrapper()
 
